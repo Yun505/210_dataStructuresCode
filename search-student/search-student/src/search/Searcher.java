14dd1341527c75ba82d15047a2fd7b06a2ce72b7
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2024-04-27 11:24:27 -0400

    last lab starter code

---
+++ search-student/search-student/src/search/Searcher.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 Marc Liberatore.
+ */
+
+package search;
+
+import java.util.List;
+
+/**
+ * An implementation of a Searcher that performs an iterative search,
+ * storing the list of next states in a Queue. This results in a
+ * breadth-first search.
+ * 
+ * @author liberato
+ *
+ * @param <T> the type for each vertex in the search graph
+ */
+public class Searcher<T> {
+	private final SearchProblem<T> searchProblem;
+	
+	/**
+	 * Instantiates a searcher.
+	 * 
+	 * @param searchProblem
+	 *            the search problem for which this searcher will find and
+	 *            validate solutions
+	 */
+	public Searcher(SearchProblem<T> searchProblem) {
+		this.searchProblem = searchProblem;
+	}
+
+	/**
+	 * Finds and return a solution to the problem, consisting of a list of
+	 * states.
+	 * 
+	 * The list should start with the initial state of the underlying problem.
+	 * Then, it should have one or more additional states. Each state should be
+	 * a successor of its predecessor. The last state should be a goal state of
+	 * the underlying problem.
+	 * 
+	 * If there is no solution, then this method should return an empty list.
+	 * 
+	 * @return a solution to the problem (or an empty list)
+	 */
+	public List<T> findSolution() {		
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Checks that a solution is valid.
+	 * 
+	 * THIS METHOD DOES NOT PERFORM SEARCH! It only checks if a provided solution
+	 * is valid!
+	 * 
+	 * A valid solution consists of a list of states. The list should start with
+	 * the initial state of the underlying problem. Then, it should have one or
+	 * more additional states. Each state should be a successor of its
+	 * predecessor. The last state should be a goal state of the underlying
+	 * problem.
+	 * 
+	 * @param solution
+	 * @return true iff this solution is a valid solution
+	 * @throws NullPointerException
+	 *             if solution is null
+	 */
+	public final boolean isValidSolution(List<T> solution) {
+		// TODO
+		return false;
+	}
+}
